50139f34f2b72ba0414506f210788fff491b1b3a
DM <damiano.mancini1@gmail.com>
2021-03-29 15:26:27 +0200

    up

--- chapter_3/80.py
+++ chapter_3/80.py
@@ -1,9 +1,11 @@
 # Prime Factors
 n = int(input("Enter an integer (2 or greater): "))
+div_prime = []
 factor = 2
-while factor < n:
+while factor <= n:
     if n % factor == 0:
+        div_prime.append(factor)
         n = n // factor
     else:
         factor += 1
-    print(factor)
+print(div_prime)
